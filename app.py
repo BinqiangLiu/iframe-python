70b9e0c893cbccdc8422786826215353f68428aa
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-12 09:56:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,20 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # 使用Selenium打开index.html并隐藏地址栏
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # 隐藏浏览器窗口
-    chrome_options.add_argument("--disable-infobars")  # 隐藏地址栏
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get('index.html')  # 替换为您的index.html文件路径
-
-    # 获取index.html的内容
-    content = driver.page_source
-
-    # 关闭浏览器
-    driver.quit()
-
-    return render_template('index.html', content=content)
+    return render_template('index.html')    
 
 if __name__ == '__main__':
    # app.run()
