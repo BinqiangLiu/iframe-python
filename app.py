b138acdd911110690498a74f8d7d0d9ea0ea10e0
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-12 09:18:24 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # 使用Selenium打开index.html并隐藏地址栏
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # 隐藏浏览器窗口
+    chrome_options.add_argument("--disable-infobars")  # 隐藏地址栏
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('./private/index.html')  # 替换为您的index.html文件路径
+
+    # 获取index.html的内容
+    content = driver.page_source
+
+    # 关闭浏览器
+    driver.quit()
+
+    return render_template('./private/index.html', content=content)
+
+if __name__ == '__main__':
+    app.run()
