4681af0a00acce118c909a90258d6a9f091e4396
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-12 09:35:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def index():
     return render_template('index.html', content=content)
 
 if __name__ == '__main__':
-    app.run()
+   # app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
